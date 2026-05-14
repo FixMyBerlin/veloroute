7936929f5b41cd20a7dd6ae6fded0e7f8ffbd08c
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-05-14 17:38:08 +0200

    Update GITHUB_REPO_NAME

--- config/config.ts
+++ config/config.ts
@@ -9,7 +9,7 @@ export const USE_MATOMO: boolean = false
 export const BASE_CONFIG = {
   CMS_NAME: 'Veloroute',
   CMS_LOGO_PATH: '/icons/icon-48x48.png',
-  GITHUB_REPO_NAME: 'rsv-veloroute',
+  GITHUB_REPO_NAME: 'veloroute',
   PRODUCTION_URL: 'https://veloroute.example.de',
   META,
   USE_MATOMO,
